1ffa72f921a9edb24d0ffe16e9d43be066ca6cde
BWizard06 <117916203+homeparty-me@users.noreply.github.com>
2023-01-13 23:09:32 +0100

    Projektdateien hinzufügen.

---
+++ BackgroundGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Runtime.InteropServices;
+using System.Transactions;
+
+namespace ChangeBackground
+{
+    class backgroundChanger
+    {
+        // Use to set a wallpaper
+        public const int SPI_SETDESKWALLPAPER = 20;
+        public const int SPIF_UPDATEINFILE = 1;
+        public const int SPIF_SENDCHANGE = 2;
+
+        [DllImport("user32.dll", CharSet=CharSet.Auto, SetLastError = true)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Random random = new Random();
+                int index = random.Next(0, 6);
+                String[] imageNames = { "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg" };
+                String imagePath = @"C:\users\benbr\Desktop\Bikes\" + imageNames[index];
+
+                // Set wallpaper
+           
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINFILE | SPIF_SENDCHANGE);
+                Thread.Sleep(3000);
+            }
+            
+
+        }
+    }
+}
